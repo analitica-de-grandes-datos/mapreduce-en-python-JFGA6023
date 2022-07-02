54fd045a2988e1045e724c6c08b60f6949112c5c
JFGA6023 <102394148+JFGA6023@users.noreply.github.com>
2022-07-02 17:12:18 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+if __name__ == '__main__':
+
+    for row in sys.stdin:
+        data= row.strip().split('   ')
+        c1=data[0]
+
+        sys.stdout.write(f'{c1}\t1\n')#
