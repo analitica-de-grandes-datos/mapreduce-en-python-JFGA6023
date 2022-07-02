bd6b745984965a2a119eae765e29feab9c9b8140
JFGA6023 <102394148+JFGA6023@users.noreply.github.com>
2022-07-02 14:42:45 -0500

    Update reducer.py

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import sys
+
+if __name__ == '__main__':
+
+    actkey = None
+    total = 0
+    for line in sys.stdin:
+
+        key, val = line.split('\t')
+        val = int(val)
+
+        if key == actkey:
+            total += val
+        else:
+            if actkey is not None:
+                sys.stdout.write('{}\t{}\n'.format(actkey, total))
+
+            actkey = key
+            total = val
+
+    sys.stdout.write("{}\t{}\n".format(actkey, total))
