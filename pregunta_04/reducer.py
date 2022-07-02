a2a0d24ff840c1f303701dac53e51731651520da
JFGA6023 <102394148+JFGA6023@users.noreply.github.com>
2022-07-02 17:12:27 -0500

    Update reducer.py

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from curses import keyname
+import sys
+
+if __name__ == '__main__':
+
+    Keyname = None
+    ttl = 0
+
+
+    for line in sys.stdin:
+
+        key, valor = line.split('\t')
+        valor = int(valor)
+
+        if key == Keyname:
+            ttl += valor
+        else:
+            if Keyname is not None:
+                
+                sys.stdout.write('{},{}\n'.format(Keyname, ttl))
+
+            Keyname = key
+            ttl = valor   
