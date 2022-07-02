823a97ebf01ef94f1859b8169d85c6c557ca03fb
JFGA6023 <102394148+JFGA6023@users.noreply.github.com>
2022-07-02 16:45:12 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+import sys
+
+if __name__ == '__main__':
+
+    for row in sys.stdin:
+        campo = row.split(',')
+        prop = campo[3]
+        mont = campo[4]
+
+        sys.stdout.write(f'{prop}\t{mont}\n')#
